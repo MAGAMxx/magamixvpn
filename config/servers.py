87d38dbad129b8535521aeacc5298b6d560b65a9
MAGAMxx <mohammadakubov@gmail.com>
2026-02-18 23:27:27 +0300

    Add files via upload

--- config/servers.py
+++ config/servers.py
@@ -6,8 +6,8 @@ RU_HIDDIFY_CLIENT_PATH = "https://ru.tgflovv.ru/UN4BySzmdiXBdptRdbhdGX"
 RU_API_KEY = "cce9292c-559f-4037-9da5-2d3d300f6dbd"
 
 # Сервер Нидерландов
-NL_HIDDIFY_ADMIN_PATH = "https://nl.tgflovv.ru/0dgEs5qApzPv4TXAhrjGH1tchLvV"
-NL_HIDDIFY_CLIENT_PATH = "https://nl.tgflovv.ru/jbNfX8qflSf4"
+NL_HIDDIFY_ADMIN_PATH = "https://vnp.tgflovv.ru/0dgEs5qApzPv4TXAhrjGH1tchLvV"
+NL_HIDDIFY_CLIENT_PATH = "https://vpn.tgflovv.ru/jbNfX8qflSf4"
 NL_API_KEY = "d7b2a687-747e-46f6-b736-cf695e67681f"
 
 # Конфигурация серверов
